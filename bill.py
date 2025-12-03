5c5246965d5f37f06478c1c3f0b1673394812ee8
vaishnavmore2004 <morevaishnav70@gmail.com>
2025-12-03 11:36:22 +0530

    Bill

---
+++ bill.py
@@ -0,0 +1,10 @@
+piz=int(input())
+puf=int(input())
+cool=int(input())
+pizrate=piz*100
+pufrate=puf*20
+coolrate=cool*10
+print(f"Price for Pizza {pizrate}")
+print(f"Price for Puff {pufrate}")
+print(f"Price for Cool Drink {coolrate}")
+print(pizrate+pufrate+coolrate)
